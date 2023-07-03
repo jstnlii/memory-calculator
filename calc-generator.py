941312c8682b8633d78339b2a9874f88e1da2253
kian <kian@pop-os.localdomain>
2023-07-02 23:45:02 -0400

    generate C program

---
+++ calc-generator.py
@@ -0,0 +1,9 @@
+	buffer = ""
+	for i in range(100):
+		for j in range(100):
+		buffer += f"\nif (a == {i} && b == {j}) {"
+		buffer += f"\n	return {i+j"
+		buffer += f"\n}"
+with open("calculator.c", "w") as file:
+	file.write(buffer)
+	
