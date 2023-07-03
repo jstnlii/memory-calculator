14be87ccbafc9c53f1baf15d45a9ac2b8f709395
kian <kian@pop-os.localdomain>
2023-07-03 00:22:11 -0400

    working on main

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-		
+	printf("Enter operation, e.g. (1+1):\n");
+	
+	int a = stoi(strtok(question, " "));
+	str operation = strtok(question, " ");
+	int b = stoi(strtok(question, " "));
+	
+	switch (operation) {
+		case "+":
 
-		
-    printf("Hello, World!\n");
-    return 0;
+			
+	return 0;
 }
 
-
+	
 
 
