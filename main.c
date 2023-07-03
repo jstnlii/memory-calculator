c84f1f3cc55df75f4316ab8ab9dc1977dcfea0a8
kian <kian@pop-os.localdomain>
2023-07-03 01:02:44 -0400

    shits fucked up

--- main.c
+++ main.c
@@ -7,12 +7,24 @@
 int main() {
 	printf("Enter operation, e.g. (1 + 1):\n");
 	
-	char question[100];
-	fgets(question, sizeof(question), stdin);
+	char question[] = "2 + 3";
 
+	/*
+	failed attempts to get user input below:
+	scanf("%s", &question);
+	gets(question);
+	fgets(question, sizeof(question), stdin);
+	*/
+	
+	printf("user input success\n");
+	a
+	for (int i = 0; i < 999; i++) {
+		
 	int a = atoi(strtok(question, "+-*/"));
 	char* operation = strtok(NULL, "+-*/");
 	int b = atoi(strtok(NULL, "+-*/"));
+
+	printf("input conversion success\n");
 
 	int result = 0;
 	switch (*operation) {
@@ -23,7 +35,7 @@ int main() {
 			//result = sub(a, b);
 			break;
 		case '*':
-			//result = mult(a, b);
+			result = mult(a, b);
 			break;
 		case '/':
 			//result = vid(a, b);
